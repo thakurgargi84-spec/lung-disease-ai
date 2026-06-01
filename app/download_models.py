a3f9a3d842ae1255f0608dbbae47bda2a8ad68fd
Gargi Thakur <thakurgargi84@gmail.com>
2026-06-02 00:02:08 +0530

    Fix model download

--- app/download_models.py
+++ app/download_models.py
@@ -8,14 +8,14 @@ seg_model_path = "saved_models/segmentation_model.h5"
 
 if not os.path.exists(cnn_model_path):
     gdown.download(
-        "https://drive.google.com/uc?id=1ZTxIAu8OgSfinMyneA7s7fLayk7DXOmA",
-        cnn_model_path,
+        id="1ZTxIAu8OgSfinMyneA7s7fLayk7DXOmA",
+        output=cnn_model_path,
         quiet=False
     )
 
 if not os.path.exists(seg_model_path):
     gdown.download(
-        "https://drive.google.com/uc?id=1a-jRs-s52GKcbWcbBTtIjs47GTJK5Pvk",
-        seg_model_path,
+        id="1a-jRs-s52GKcbWcbBTtIjs47GTJK5Pvk",
+        output=seg_model_path,
         quiet=False
     )
